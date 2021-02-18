c52364393480300a0efe55d546a1e59ebcea2a8a
Rawat, Arvind <arvind.rawat@intel.com>
2021-02-18 17:30:31 +0530

    v3.5/task/isecl-14147: Added code for parsing subject from long lived token and setting it in http context.

--- context/context.go
+++ context/context.go
@@ -12,32 +12,26 @@ import (
 	types "intel/isecl/lib/common/v3/types/aas"
 )
 
-type httpContextKey string
-
-var userRoleKey = httpContextKey("userroles")
-var userPermissionKey = httpContextKey("userpermissions")
+const (
+	UserRoles       = "userroles"
+	UserPermissions = "userpermissions"
+	TokenSubject    = "tokensubject"
+)
 
 func SetUserRoles(r *http.Request, val []types.RoleInfo) *http.Request {
 
-	ctx := context.WithValue(r.Context(), "userroles", val)
+	ctx := context.WithValue(r.Context(), UserRoles, val)
 	return r.WithContext(ctx)
 }
 
 func SetUserPermissions(r *http.Request, val []types.PermissionInfo) *http.Request {
 
-	ctx := context.WithValue(r.Context(), "userpermissions", val)
+	ctx := context.WithValue(r.Context(), UserPermissions, val)
 	return r.WithContext(ctx)
 }
 
-/*
- func SetUserRoles(r *http.Request, val types.Roles) *http.Request {
-
-	 ctx := context.WithValue(r.Context(), userRoleKey, val)
-	 return r.WithContext(ctx)
- }
-*/
 func GetUserRoles(r *http.Request) ([]types.RoleInfo, error) {
-	if rv := r.Context().Value("userroles"); rv != nil {
+	if rv := r.Context().Value(UserRoles); rv != nil {
 		if ur, ok := rv.([]types.RoleInfo); ok {
 			return ur, nil
 		}
@@ -46,10 +40,25 @@ func GetUserRoles(r *http.Request) ([]types.RoleInfo, error) {
 }
 
 func GetUserPermissions(r *http.Request) ([]types.PermissionInfo, error) {
-	if rv := r.Context().Value("userpermissions"); rv != nil {
+	if rv := r.Context().Value(UserPermissions); rv != nil {
 		if ur, ok := rv.([]types.PermissionInfo); ok {
 			return ur, nil
 		}
 	}
 	return nil, fmt.Errorf("could not retrieve user permissions from context")
+}
+
+func SetTokenSubject(r *http.Request, val string) *http.Request {
+
+	ctx := context.WithValue(r.Context(), TokenSubject, val)
+	return r.WithContext(ctx)
+}
+
+func GetTokenSubject(r *http.Request) (string, error) {
+	if rv := r.Context().Value(TokenSubject); rv != nil {
+		if ur, ok := rv.(string); ok {
+			return ur, nil
+		}
+	}
+	return "", fmt.Errorf("could not retrieve token subject from context")
 }
